2be39bf886c4e31e007aed15024d752fd8911a46
John Mordi <johnmordi00@gmail.com>
2021-02-17 17:23:40 -0600

    Added basic player controller and adjusted scene

---
+++ MonkeyMayhem/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(Vector3.left * 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector3.back * 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector3.right * 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+        }
+    }
+}
